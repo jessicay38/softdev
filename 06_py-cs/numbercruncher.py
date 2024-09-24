2991078fc0f80f4b24d821bf1670783a0070f7d9
jyu50 <144478356+jessicay38@users.noreply.github.com>
2024-09-23 22:09:54 -0400

    Update numbercruncher.py

--- 06_py-cs/numbercruncher.py
+++ 06_py-cs/numbercruncher.py
@@ -1,12 +1,25 @@
 #Jessica Yu
+#Cracked
 #Softdev
 #K06 - Dictionary from CSV file
 #2024-9-19
-#time spent:
+#time spent: 20 mins
 
-import csv
 import random
-dictionary = {}
+import csv
 
-with open('occupations.csv', newline=' ') as csvfile:
-    occupations = csv.reader(csvfile) 
+list1=[]
+with open('occupations.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        list1.append(row)
+print(list1)
+percentage=[]
+job=[]
+for dict1 in list1:
+    percentage.append(float(dict1.get("Percentage")))
+#print(percentage)
+for dict1 in list1:
+    job.append(dict1.get("Job Class"))
+
+print(random.choices(job[1:-1], weights=percentage[1:-1]))
