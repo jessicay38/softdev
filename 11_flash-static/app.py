19304e2d4b2a7787bdc77793d207799b8859bbbc
Jessica <jyu50@stuy.edu>
2024-09-26 11:40:52 -0400

    caught up on work

--- 11_flash-static/app.py
+++ 11_flash-static/app.py
@@ -14,14 +14,19 @@ def hello_world():
     print(__name__)
     return "No hablo queso!"
 
-'''
+
 @app.route("/static/foo.html")
 def h():
     print("the __name__ of this module is... ")
     print(__name__)
     return str(random.random())
-'''
+
+@app.route("static/fixie.html")
+def fix():
+    return "why does this keeping this open return an error?"
+
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
+    
