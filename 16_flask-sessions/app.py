becff6fbaad5576799dd80716dae0f5ea5c9acae
Jessica <jyu50@stuy.edu>
2024-10-11 07:50:13 -0400

    updated files

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,59 +1,39 @@
-# Jessica Yu, Stella Yampolsky, Tracey Ye
+# Jessica Yu
 # JST
+# K16 - Building onto the flask website 
 # SoftDev
-# K16 - Building on K15 with user logout
-# October 9, 2024
+# Oct 09, 2024
 # Time Spent: 
-
-# import conventions:
-# list most general first (standard python library)
-# ...then pip installs (eg Flask)
-# ...then your own home-rolled modules/packages (today's test module)
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
-
-#import testmod0
-
-#the conventional way:
-#from flask import Flask, render_template, request
+from flask import session
+import os
 
 app = Flask(__name__)    #create Flask object
+app.secret_key = os.urandom(32)
 
-
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
- * Some will work as written;
- *  ...other sections will not. 
-
-TASK:
- Predict which.
- 1. Devise simple tests to isolate components/behaviors.
- 2. Execute your tests.
- 3. Process results.
- 4. Findings yield new ideas for more tests? Yes: do them.
-
-PROTIP: Insert your own in-line comments
- wherever they will help
-  your future self and/or current teammates
-   understand what is going on.
-'''
-
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/")
 def disp_loginpage():
-    return render_template( 'login.html' )
+    if request.cookies.get('username') is None:
+        return render_template( 'login.html' )
+    else:
+        return render_template("response.html", r = request.cookies.get('username'), method = request.method)
 
-
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/resp", methods=['GET', 'POST'])
 def response():
-    return render_template('response.html', response = request.args['username'], method = request.method) #response to a form submission
-
-
+    if request.method == "GET":
+        session['username'] = request.args['username']
+        return render_template("response.html", r = request.args['username'], method = request.method) #response to a form submission
+    if request.method == "POST":
+        session['username'] = request.form['username']
+        return render_template("response.html", r = request.form['username'], method = request.method) #response to a form submission
     
-if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    return render_template("logout.html")
+    
+if __name__ == "__main__": 
     app.debug = True 
     app.run()
